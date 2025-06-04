fce8b31ad70a9bea38f090a67939ce419f28e486
xnkhi <earthpig97@gmail.com>
2025-06-04 13:54:38 -0400

    uwu

--- src/airootfs/root/customize_airootfs.sh
+++ src/airootfs/root/customize_airootfs.sh
@@ -13,14 +13,14 @@ chmod +x WhiteSur-gtk-theme/install.sh
 chmod +x WhiteSur-gtk-theme/tweaks.sh
 echo "Running Whitesur Installer"
 ./WhiteSur-gtk-theme/install.sh -d /usr/share/themes -l -n Taeyang -c light -t red --shell -i simple --silent-mode
-./WhiteSur-gtk-theme -g -b "/etc/default-wallpaper/wallpaper.png"
-rm -rf Whitesur-gtk-theme
+./WhiteSur-gtk-theme/tweaks.sh -g -b "/etc/default-wallpaper/wallpaper.png"
+rm -rf WhiteSur-gtk-theme
 
 
 ### icon theme ###
 
 git clone https://github.com/vinceliuice/WhiteSur-icon-theme
-chmod +x Whitesur-icon-theme/install.sh
-echo "Running Whitesur Icons Installer"
+chmod +x WhiteSur-icon-theme/install.sh
+echo "Running WhiteSur Icons Installer"
 ./Whitesur-icon-theme/install.sh -d /usr/share/icons -n Taeyang-Icons -t red -a
-rm -rf Whitesur-icon-theme
+rm -rf WhiteSur-icon-theme
